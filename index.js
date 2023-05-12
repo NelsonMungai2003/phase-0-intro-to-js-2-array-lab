84b9bdb4ed146322809c9d93ca0529b73cdbc266
Nelson <mungainelle96@gmail.com>
2023-05-12 16:50:03 +0300

    passed all nine test

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 // Write your solution here!
+const cats=['Milo','Otis','Garfield']
+function destructivelyAppendCat(name){
+    cats.push(name)
+}
+function destructivelyPrependCat(name){
+    cats.unshift(name)
+}
+
+function destructivelyRemoveLastCat(){
+    cats.pop()
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+}
+function appendCat(name){
+    
+    return[...cats.slice(),name]
+
+}
+function prependCat(name){
+    let copyCats=[];
+    copyCats=[name,...cats.slice()]
+    return copyCats
+
+}
+function removeLastCat(){
+    let copyCats=[]
+    copyCats=cats.slice(0,-1)
+    return copyCats
+}
+function removeFirstCat(){
+    let copyCats=[]
+    copyCats=cats.slice(1)
+    return copyCats
+}
